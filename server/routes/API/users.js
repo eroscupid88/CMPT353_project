8d499e508bed5414e97c5bfa80bc300ec79c3bca
Eroscupid <vutuanduong2003@yahoo.com>
2021-10-13 17:51:25 -0600

    working on user API

--- server/routes/API/users.js
+++ server/routes/API/users.js
@@ -1,4 +1,27 @@
-const express = require('express');
-const router = express.Router();
+// Router
+const router = require('express').Router();
+
+const Users = require('../../model/user');
+
+/**
+ *  Post Request to register a new User
+ *  Path: /api/user/register
+ */
+router.post('/register', (req, res, next) => {
+  // TODO
+});
+/**
+ *  POST request to login user
+ *  Path: /api/user/login
+ */
+router.post('/login', (req, res, next) => {
+  // TODO
+});
+/**
+ *  GET request to get current user
+ */
+router.get('/current', (req, res) => {
+  // TODO
+});
 
 module.exports = router;
