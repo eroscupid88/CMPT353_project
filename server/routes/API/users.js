2431d89bc7de2e5cf157acb1834024707fc33fdb
Eroscupid <vutuanduong2003@yahoo.com>
2021-11-01 10:47:46 -0600

    add login POST method to check email and password

--- server/routes/API/users.js
+++ server/routes/API/users.js
@@ -6,10 +6,11 @@ const mongoose = require('mongoose');
 const Users = require('../../model/user');
 // validate register input
 const validateRegisterInputs = require('../../validation/register');
+const validateLoginInputs = require('../../validation/login');
 
 /**
  *  Post Request to register a new User
- *  Path: /api/user/register
+ *  Path: v1/api/user/register
  */
 router.post('/register', (req, res, next) => {
   const { errors, isValid } = validateRegisterInputs(req.body);
@@ -19,9 +20,9 @@ router.post('/register', (req, res, next) => {
   Users.find({ email: req.body.email })
     .exec()
     .then((user) => {
-      if (user.length > 1) {
+      if (user.length >= 1) {
         errors.email = 'Email exist';
-        return res.status(409).json(errors);
+        return res.status(409).json({ error: errors });
       }
       const defaultAvatar =
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqlaxdI3EIEf2voJ56Ut0G_M9pXJhUYgQXQaKLMzL5yJ81cbUt';
@@ -42,16 +43,53 @@ router.post('/register', (req, res, next) => {
               info: result,
             },
           });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            error: err,
+          });
         });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 /**
  *  POST request to login user
- *  Path: /api/user/login
+ *  Path: v1/api/user/login
  */
 router.post('/login', (req, res, next) => {
-  // TODO
-  res.json({ body: req.body });
+  const { errors, isValid } = validateLoginInputs(req.body);
+  if (!isValid) {
+    return res.status(409).json(errors);
+  }
+  Users.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      // check if user is already in database or not. return errors with status 401 if not found
+      if (user.length < 1) {
+        errors.email = ' User not found';
+        return res.status(401).json(errors);
+      }
+      if (user[0].password !== req.body.password) {
+        return res.status(500).json({
+          message: 'Auth fail!',
+        });
+      }
+      res.status(200).json({
+        message: 'Success login',
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 /**
  *  GET request to get current user
--- server/validation/login.js
+++ server/validation/login.js
@@ -1,10 +1,24 @@
-const validate = require('validator');
+const validator = require('validator');
 const isEmpty = require('./isempty');
 
-module.exports = function validateLoginInput(data) {
+module.exports = function validateLoginInputs(data) {
   const errors = {};
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  console.log(data.email);
+  console.log(data.password);
+
+  if (!validator.isEmail(data.email)) {
+    errors.email = 'input is not an email type';
+  }
+  if (validator.isEmpty(data.email)) {
+    errors.email = 'Email field cannot be empty';
+  }
+  if (validator.isEmpty(data.password)) {
+    errors.password = 'password field cannot be empty';
+  }
   return {
-    error: errors,
+    errors,
     isValid: isEmpty(errors),
   };
 };
