68c442fe63a39718c00d5c79feeec179c0f11d30
Eroscupid <vutuanduong2003@yahoo.com>
2021-11-01 13:06:23 -0600

    add to dillon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, HashRouter, Route, Switch } from 'react-router-dom'
 import './scss/style.scss'
 
 const loading = (
@@ -16,26 +16,21 @@ const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+const About = React.lazy(() => import('./components/about/About'))
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <React.Suspense fallback={loading}>
-          <Switch>
-            <Route exact path="/login" name="Login Page" render={(props) => <Login {...props} />} />
-            <Route
-              exact
-              path="/register"
-              name="Register Page"
-              render={(props) => <Register {...props} />}
-            />
-            <Route exact path="/404" name="Page 404" render={(props) => <Page404 {...props} />} />
-            <Route exact path="/500" name="Page 500" render={(props) => <Page500 {...props} />} />
-            <Route path="/" name="Home" render={(props) => <DefaultLayout {...props} />} />
-          </Switch>
-        </React.Suspense>
-      </HashRouter>
+      <React.Suspense fallback={loading}>
+        <Router>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" name="Register Page" component={Register} />
+          <Route exact path="/404" name="Page 404" component={Page404} />
+          <Route exact path="/500" name="Page 500" component={Page500} />
+          <Route exact path="/about" name="About" component={About} />
+          <Route path="/dashboard" name="Home" component={DefaultLayout} />
+        </Router>
+      </React.Suspense>
     )
   }
 }
