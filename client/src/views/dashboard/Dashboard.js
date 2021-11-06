217f8cd96b62a12b2bbf20f06fc28f0d9bd1aa52
Eroscupid <vutuanduong2003@yahoo.com>
2021-11-06 16:22:37 -0600

    do somechange to profile picture

--- client/src/views/dashboard/Dashboard.js
+++ client/src/views/dashboard/Dashboard.js
@@ -1,9 +1,34 @@
 import React, { lazy, Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 class Dashboard extends Component {
+  constructor() {
+    super()
+    this.state = {
+      auth: 'sfasdfsad',
+      a: 'hohoho',
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.setState({ auth: this.props.auth })
+    }
+  }
+
   render() {
-    return <></>
+    return (
+      <>
+        <h1>{this.props.auth.user.email}</h1>
+      </>
+    )
   }
 }
-
-export default Dashboard
+Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired,
+}
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+})
+export default connect(mapStateToProps)(Dashboard)
