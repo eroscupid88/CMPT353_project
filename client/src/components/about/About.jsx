23ea8bd07899b3051017e86a6b37e659371f43c4
lucius-95 <77852558+lucius-95@users.noreply.github.com>
2021-11-06 19:11:41 -0600

    changed avatar

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -142,15 +142,18 @@ class About extends Component {
                 <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                   <div className="our-team">
                     <div className="picture">
-                      <img className="img-fluid" src="https://picsum.photos/130/130?image=1027" />
+                      <img
+                        className="img-fluid"
+                        src="https://cdn.discordapp.com/attachments/904612910385467412/906711303089963008/IMG_0463.jpg"
+                      />
                     </div>
                     <div className="team-content">
-                      <h3 className="name">Michele Miller</h3>
-                      <h4 className="title">Web Developer</h4>
+                      <h3 className="name">Lucius Ho</h3>
+                      <h4 className="title">Software Engineer Student</h4>
                     </div>
                     <ul className="social">
                       <li>
-                        TiSocialTwitter
+                        Discord
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-facebook"
