481310cf9291ca5392d5957ab8f7f0615ef6fc27
lucius-95 <77852558+lucius-95@users.noreply.github.com>
2021-11-08 16:32:45 -0600

    changed profiles

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -20,99 +20,25 @@ class About extends Component {
         <section>
           <div className="block">
             <p>
-              <span className="first-character sc">L</span>orem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Orci a scelerisque purus semper eget duis. Sit amet facilisis magna etiam tempor.
-              Viverra nam libero justo laoreet. Aliquet nec ullamcorper sit amet risus. Arcu felis
-              bibendum ut tristique et egestas. Vulputate eu scelerisque felis imperdiet proin
-              fermentum leo vel orci. Diam vulputate ut pharetra sit amet aliquam id diam maecenas.
-              Justo laoreet sit amet cursus sit. Aliquam id diam maecenas ultricies mi eget.
-              Elementum nibh tellus molestie nunc non. In eu mi bibendum neque. A lacus vestibulum
-              sed arcu non odio. Suspendisse faucibus interdum posuere lorem ipsum dolor sit amet.
-              Elementum sagittis vitae et leo duis ut. Sagittis aliquam malesuada bibendum arcu
-              vitae. Vulputate enim nulla aliquet porttitor lacus luctus accumsan. Vitae congue eu
-              consequat ac felis donec. Rutrum quisque non tellus orci ac auctor augue mauris augue.
-              Eu consequat ac felis donec et odio. Hac habitasse platea dictumst vestibulum rhoncus
-              est pellentesque elit ullamcorper. Porttitor lacus luctus accumsan tortor posuere ac
-              ut consequat semper. Blandit cursus risus at ultrices mi tempus imperdiet. Hac
-              habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Nisl purus in
-              mollis nunc sed. Purus ut faucibus pulvinar elementum integer. Mauris ultrices eros in
-              cursus turpis massa tincidunt dui. Porta lorem mollis aliquam ut. Tellus id interdum
-              velit laoreet id. Nunc mattis enim ut tellus elementum sagittis. Vel pretium lectus
-              quam id leo in vitae turpis. Lacus vel facilisis volutpat est velit egestas dui id.
-              Turpis egestas integer eget aliquet nibh praesent tristique magna. Laoreet suspendisse
-              interdum consectetur libero id faucibus nisl tincidunt. Eget magna fermentum iaculis
-              eu non. Neque convallis a cras semper auctor neque vitae tempus quam. Iaculis eu non
-              diam phasellus vestibulum lorem sed risus. Fringilla phasellus faucibus scelerisque
-              eleifend donec pretium vulputate sapien nec. Amet volutpat consequat mauris nunc
-              congue nisi vitae. Massa id neque aliquam vestibulum. Ultricies leo integer malesuada
-              nunc vel. Ut aliquam purus sit amet luctus venenatis lectus. Nec feugiat nisl pretium
-              fusce. Enim ut sem viverra aliquet eget sit amet. Interdum consectetur libero id
-              faucibus nisl tincidunt eget. Metus vulputate eu scelerisque felis imperdiet. Libero
-              id faucibus nisl tincidunt eget nullam non. Cras sed felis eget velit aliquet sagittis
-              id consectetur. Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Arcu
-              dictum varius duis at consectetur lorem. Mi quis hendrerit dolor magna eget est. Neque
-              vitae tempus quam pellentesque nec. Sit amet est placerat in egestas erat imperdiet
-              sed. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Aliquet enim
-              tortor at auctor. Non quam lacus suspendisse faucibus. Neque laoreet suspendisse
-              interdum consectetur libero id faucibus nisl tincidunt. Ultrices eros in cursus turpis
-              massa tincidunt. Porttitor lacus luctus accumsan tortor. Egestas pretium aenean
-              pharetra magna ac placerat vestibulum. Aliquam eleifend mi in nulla posuere
-              sollicitudin aliquam ultrices sagittis. In arcu cursus euismod quis viverra nibh cras.
-              Iaculis eu non diam phasellus vestibulum. Libero volutpat sed cras ornare. Ipsum dolor
-              sit amet consectetur adipiscing elit. Sit amet mauris commodo quis imperdiet massa
-              tincidunt nunc. Sit amet massa vitae tortor condimentum lacinia. Faucibus a
-              pellentesque sit amet porttitor eget. Rhoncus aenean vel elit scelerisque mauris
-              pellentesque pulvinar pellentesque. Ullamcorper dignissim cras tincidunt lobortis
-              feugiat vivamus at. Libero id faucibus nisl tincidunt eget nullam non. Tortor at
-              auctor urna nunc. Cras fermentum odio eu feugiat pretium nibh ipsum. Nec dui nunc
-              mattis enim ut tellus elementum sagittis vitae. Fermentum odio eu feugiat pretium nibh
-              ipsum consequat. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin
-              aliquam. Dui faucibus in ornare quam. Viverra ipsum nunc aliquet bibendum enim
-              facilisis gravida neque. Scelerisque mauris pellentesque pulvinar pellentesque
-              habitant. Commodo elit at imperdiet dui accumsan sit amet nulla. Duis ultricies lacus
-              sed turpis tincidunt id. Mauris sit amet massa vitae tortor condimentum lacinia quis.
-              Nam at lectus urna duis.
+              <span className="first-character sc">I</span>ntroduction paragraph here
             </p>
-            <p className="line-break margin-top-10"></p>
-            <p className="margin-top-10">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Consequat mauris nunc congue nisi. Justo
-              donec enim diam vulputate ut pharetra sit amet. Consectetur adipiscing elit duis
-              tristique sollicitudin nibh. Eleifend mi in nulla posuere sollicitudin aliquam
-              ultrices sagittis orci. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien.
-              Neque egestas congue quisque egestas diam in arcu cursus euismod. Arcu bibendum at
-              varius vel pharetra vel turpis nunc eget. Id donec ultrices tincidunt arcu non sodales
-              neque sodales ut. Viverra justo nec ultrices dui sapien eget mi. Id diam maecenas
-              ultricies mi eget mauris pharetra et ultrices. Morbi quis commodo odio aenean sed
-              adipiscing diam donec adipiscing. Mollis aliquam ut porttitor leo a diam sollicitudin
-              tempor.
-            </p>
+            <p className="line-break margin-top-10" />
+            <p className="margin-top-10">Second paragraph if needed</p>
           </div>
         </section>
 
         <section>
           <div className="parallax-two">
-            <h2>WHERE ARE WE COME FROM</h2>
+            <h2>WHERE ARE WE FROM</h2>
           </div>
         </section>
 
         <section>
           <div className="block">
             <p>
-              <span className="first-character ny">L</span>orem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Consequat mauris nunc congue nisi. Justo donec enim diam vulputate ut pharetra sit
-              amet. Consectetur adipiscing elit duis tristique sollicitudin nibh. Eleifend mi in
-              nulla posuere sollicitudin aliquam ultrices sagittis orci. Commodo quis imperdiet
-              massa tincidunt nunc pulvinar sapien. Neque egestas congue quisque egestas diam in
-              arcu cursus euismod. Arcu bibendum at varius vel pharetra vel turpis nunc eget. Id
-              donec ultrices tincidunt arcu non sodales neque sodales ut. Viverra justo nec ultrices
-              dui sapien eget mi. Id diam maecenas ultricies mi eget mauris pharetra et ultrices.
-              Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Mollis aliquam ut
-              porttitor leo a diam sollicitudin tempor.
+              <span className="first-character ny">W</span>here
             </p>
-            <p className="line-break margin-top-10"></p>
+            <p className="line-break margin-top-10" />
             <p className="margin-top-10">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua. Consequat mauris nunc congue nisi. Justo
@@ -144,12 +70,12 @@ class About extends Component {
                     <div className="picture">
                       <img
                         className="img-fluid"
-                        src="https://cdn.discordapp.com/attachments/904612910385467412/906711303089963008/IMG_0463.jpg"
+                        src="https://drive.google.com/uc?id=1I0rtTbspL06jdG-N7NH9fHn-jzuH_d6g"
                       />
                     </div>
                     <div className="team-content">
                       <h3 className="name">Lucius Ho</h3>
-                      <h4 className="title">Software Engineer Student</h4>
+                      <h4 className="title">Software Engineering Student</h4>
                     </div>
                     <ul className="social">
                       <li>
@@ -158,28 +84,28 @@ class About extends Component {
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-facebook"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-twitter"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-google-plus"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-linkedin"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                     </ul>
                   </div>
@@ -187,37 +113,40 @@ class About extends Component {
                 <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                   <div className="our-team">
                     <div className="picture">
-                      <img className="img-fluid" src="https://picsum.photos/130/130?image=839" />
+                      <img
+                        className="img-fluid"
+                        src="https://drive.google.com/uc?id=1Ydes4r4XvFRuTkzOmoT0-MSLTFfi8O_U"
+                      />
                     </div>
                     <div className="team-content">
-                      <h3 className="name">Patricia Knott</h3>
-                      <h4 className="title">Web Developer</h4>
+                      <h3 className="name">Dillon Vu</h3>
+                      <h4 className="title">Software Engineering Student</h4>
                     </div>
                     <ul className="social">
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-facebook"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-twitter"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-google-plus"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-linkedin"
-                        ></a>
+                        />
                       </li>
                     </ul>
                   </div>
@@ -225,11 +154,14 @@ class About extends Component {
                 <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                   <div className="our-team">
                     <div className="picture">
-                      <img className="img-fluid" src="https://picsum.photos/130/130?image=856" />
+                      <img
+                        className="img-fluid"
+                        src="https://drive.google.com/uc?id=1ES99zId-DRjk3EwzSLcUNOzxat0tTHU2"
+                      />
                     </div>
                     <div className="team-content">
-                      <h3 className="name">Justin Ramos</h3>
-                      <h4 className="title">Web Developer</h4>
+                      <h3 className="name">Kyle Holtby</h3>
+                      <h4 className="title">Software Engineering Student</h4>
                     </div>
                     <ul className="social">
                       <li>
@@ -237,28 +169,28 @@ class About extends Component {
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-facebook"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-twitter"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-google-plus"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-linkedin"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                     </ul>
                   </div>
@@ -278,28 +210,28 @@ class About extends Component {
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-facebook"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-twitter"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-google-plus"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                       <li>
                         <a
                           href="https://codepen.io/collection/XdWJOQ/"
                           className="fa fa-linkedin"
                           aria-hidden="true"
-                        ></a>
+                        />
                       </li>
                     </ul>
                   </div>
