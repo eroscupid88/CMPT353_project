1a28aa009acb5f64d114f6b5c5d5737fc785a731
lucius-95 <77852558+lucius-95@users.noreply.github.com>
2021-11-09 19:47:15 -0600

    Updated the about page

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,241 +1,63 @@
 import React, { Component } from 'react'
+import Paragraph from './Paragraph'
+import Member from './Member'
 
 class About extends Component {
   render() {
     return (
       <div id="parallax-world-of-ugg">
         <section>
-          <div className="title">
-            <h3>About Us</h3>
-            <h1>Nomosnow</h1>
-          </div>
+          <h1>Nomosnow</h1>
         </section>
 
         <section>
-          <div className="parallax-one">
+          <div className="parallax one">
             <h2>WHAT WE DO</h2>
           </div>
         </section>
 
         <section>
-          <div className="block">
-            <p>
-              <span className="first-character sc">I</span>ntroduction paragraph here
-            </p>
-            <p className="line-break margin-top-10" />
-            <p className="margin-top-10">Second paragraph if needed</p>
+          <Paragraph letter="F" first="irst paragraph" second="Second paragraph if needed" />
+        </section>
+
+        <section>
+          <div className="parallax two">
+            <h2>WHERE WE ARE FROM</h2>
           </div>
         </section>
 
         <section>
-          <div className="parallax-two">
-            <h2>WHERE ARE WE FROM</h2>
-          </div>
+          <Paragraph letter="F" first="irst paragraph" second="Second paragraph if needed" />
         </section>
 
         <section>
-          <div className="block">
-            <p>
-              <span className="first-character ny">W</span>here
-            </p>
-            <p className="line-break margin-top-10" />
-            <p className="margin-top-10">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Consequat mauris nunc congue nisi. Justo
-              donec enim diam vulputate ut pharetra sit amet. Consectetur adipiscing elit duis
-              tristique sollicitudin nibh. Eleifend mi in nulla posuere sollicitudin aliquam
-              ultrices sagittis orci. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien.
-              Neque egestas congue quisque egestas diam in arcu cursus euismod. Arcu bibendum at
-              varius vel pharetra vel turpis nunc eget. Id donec ultrices tincidunt arcu non sodales
-              neque sodales ut. Viverra justo nec ultrices dui sapien eget mi. Id diam maecenas
-              ultricies mi eget mauris pharetra et ultrices. Morbi quis commodo odio aenean sed
-              adipiscing diam donec adipiscing. Mollis aliquam ut porttitor leo a diam sollicitudin
-              tempor.
-            </p>
-          </div>
-        </section>
-
-        <section>
-          <div className="parallax-three">
-            <h2>SAY SOMETHING HERE</h2>
-          </div>
-        </section>
-
-        <section>
-          <div className="ourteam-bg">
+          <div className="ourTeam-bg">
             <div className="container">
               <div className="row">
-                <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                  <div className="our-team">
-                    <div className="picture">
-                      <img
-                        className="img-fluid"
-                        src="https://drive.google.com/uc?id=1I0rtTbspL06jdG-N7NH9fHn-jzuH_d6g"
-                      />
-                    </div>
-                    <div className="team-content">
-                      <h3 className="name">Lucius Ho</h3>
-                      <h4 className="title">Software Engineering Student</h4>
-                    </div>
-                    <ul className="social">
-                      <li>
-                        Discord
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-facebook"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-twitter"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-google-plus"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-linkedin"
-                          aria-hidden="true"
-                        />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                  <div className="our-team">
-                    <div className="picture">
-                      <img
-                        className="img-fluid"
-                        src="https://drive.google.com/uc?id=1Ydes4r4XvFRuTkzOmoT0-MSLTFfi8O_U"
-                      />
-                    </div>
-                    <div className="team-content">
-                      <h3 className="name">Dillon Vu</h3>
-                      <h4 className="title">Software Engineering Student</h4>
-                    </div>
-                    <ul className="social">
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-facebook"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-twitter"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-google-plus"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-linkedin"
-                        />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                  <div className="our-team">
-                    <div className="picture">
-                      <img
-                        className="img-fluid"
-                        src="https://drive.google.com/uc?id=1ES99zId-DRjk3EwzSLcUNOzxat0tTHU2"
-                      />
-                    </div>
-                    <div className="team-content">
-                      <h3 className="name">Kyle Holtby</h3>
-                      <h4 className="title">Software Engineering Student</h4>
-                    </div>
-                    <ul className="social">
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-facebook"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-twitter"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-google-plus"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-linkedin"
-                          aria-hidden="true"
-                        />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                  <div className="our-team">
-                    <div className="picture">
-                      <img className="img-fluid" src="https://picsum.photos/130/130?image=836" />
-                    </div>
-                    <div className="team-content">
-                      <h3 className="name">Mary Huntley</h3>
-                      <h4 className="title">Web Developer</h4>
-                    </div>
-                    <ul className="social">
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-facebook"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-twitter"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-google-plus"
-                          aria-hidden="true"
-                        />
-                      </li>
-                      <li>
-                        <a
-                          href="https://codepen.io/collection/XdWJOQ/"
-                          className="fa fa-linkedin"
-                          aria-hidden="true"
-                        />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
+                <Member
+                  pic="https://i.ibb.co/2hYPR7D/Dillon.jpg"
+                  name="Dillon Vu"
+                  github="https://github.com/eroscupid88"
+                  linkedin="https://www.linkedin.com/in/dillon-vu/"
+                />
+                <Member
+                  pic="https://i.ibb.co/17rQLM5/Lucius.jpg"
+                  name="Lucius Ho"
+                  github="https://github.com/lucius-95"
+                  linkedin="https://www.linkedin.com/in/lucius-ho/"
+                />
+                <Member
+                  pic="https://i.ibb.co/kKZnVxs/Kyle.jpg"
+                  name="Kyle Holtby"
+                  github="https://github.com/KyleHoltby"
+                  linkedin="https://www.linkedin.com/in/kyle-holtby-7aa152195/"
+                />
+                <Member
+                  pic="https://i.ibb.co/YPfJyMS/Hasin.jpg"
+                  name="Hasin Raihan"
+                  github="https://github.com/hasinraihan55"
+                  linkedin="https://www.linkedin.com/in/hasin-raihan-09b37b1b8/"
+                />
               </div>
             </div>
           </div>
