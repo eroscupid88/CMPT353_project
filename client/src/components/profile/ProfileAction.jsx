0346d637e9caf720aa5893ba49e28aa78d5beef0
lucius-95 <77852558+lucius-95@users.noreply.github.com>
2021-11-17 12:00:33 -0600

    Updated /welcome

--- client/src/components/profile/ProfileAction.jsx
+++ client/src/components/profile/ProfileAction.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import isEmpty from "../../validation/isEmpty";
+import isEmpty from '../../validation/isEmpty'
+import { CButton, CCol, CLink, CRow } from '@coreui/react'
 const ProfileAction = (props) => {
   let companyItem
-  if (props.user.user.email === 'dillon@yahoo.com'){
-    if (isEmpty(props.mycompany.company)){
+  if (props.user.user.email === 'dillon@yahoo.com') {
+    if (isEmpty(props.mycompany.company)) {
       companyItem = (
         <Link to="/create-company" className="btn btn-light">
           <i className="fas fa-user-circle text-info mr-1" /> Create Company
         </Link>
       )
-    }
-    else{
+    } else {
       companyItem = (
         <Link to="/edit-company" className="btn btn-light">
           <i className="fas fa-user-circle text-info mr-1" /> Edit Company
@@ -21,13 +21,21 @@ const ProfileAction = (props) => {
   }
 
   return (
-    <div className="btn-group mb-4" role="group">
-      <Link to="/edit-profile" className="btn btn-light">
-        <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
-      </Link>
-      {companyItem}
-    </div>
-
+    // <div className="btn-group mb-4" role="group">
+    //   <Link to="/edit-profile" className="btn btn-light">
+    //     <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
+    //   </Link>
+    //   {companyItem}
+    // </div>
+    <CRow>
+      <CCol xs={6}>
+        <Link to="/edit-profile">
+          <CButton color="light" className="px-4 btn-outline-info">
+            Edit Profile
+          </CButton>
+        </Link>
+      </CCol>
+    </CRow>
   )
 }
 
