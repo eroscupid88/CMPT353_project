837576bdabfdaa7b389fd182fd06a976fdb96b08
lucius-95 <77852558+lucius-95@users.noreply.github.com>
2021-11-23 11:34:56 -0600

    some minor changes

--- client/src/components/organization/Organizations.jsx
+++ client/src/components/organization/Organizations.jsx
@@ -1,48 +1,55 @@
-import React,{Component} from "react";
-import {connect} from "react-redux";
-import {getCompanies} from '../../action/companyAction'
-import PropTypes from "prop-types";
-import CompanyList from "./CompanyList";
-import Loader from "../common/Loader";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getCompanies } from '../../action/companyAction'
+import PropTypes from 'prop-types'
+import CompanyList from './CompanyList'
+import Loader from '../common/Loader'
+import { AppSidebar, AppFooter, AppHeader } from '../../components/index'
 
-class Organizations extends Component{
+class Organizations extends Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
   componentDidMount() {
     // call getCompanies to get information of the companies
     this.props.getCompanies()
   }
 
-
-  render(){
-
+  render() {
     const { companies, loading } = this.props.company
     let companiesDetails
-    if (companies == null || loading){
+    if (companies == null || loading) {
       companiesDetails = <Loader />
-    }else{
+    } else {
       if (Object.keys(companies).length > 0) {
         console.log(companies)
-        companiesDetails = <CompanyList companies = {companies} />
+        companiesDetails = <CompanyList companies={companies} />
       }
     }
-    return( (
-      <section id="companies">
-        <h1 className="page-title">Hope a better World</h1>
-        {companiesDetails}
-      </section>
-    ))
+    return (
+      <div>
+        <AppSidebar />
+        <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+          <AppHeader />
+          <div className="body flex-grow-1 px-3">
+            <section id="companies">
+              <h1 className="page-title">Hope a better World</h1>
+              {companiesDetails}
+            </section>
+          </div>
+          <AppFooter />
+        </div>
+      </div>
+    )
   }
 }
 Organizations.propTypes = {
   company: PropTypes.object.isRequired,
-  getCompanies: PropTypes.func.isRequired
+  getCompanies: PropTypes.func.isRequired,
 }
 
 const mapPropToState = (state) => ({
-  company: state.company
+  company: state.company,
 })
 
-export default connect(mapPropToState
-, {getCompanies})(Organizations)
+export default connect(mapPropToState, { getCompanies })(Organizations)
