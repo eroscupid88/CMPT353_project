ce99c6b89e6e28e545f36d4717ed0946e1d0f461
lucius-95 <77852558+lucius-95@users.noreply.github.com>
2021-11-23 18:26:31 -0600

    Update Donation.jsx

--- client/src/components/organization/donation/Donation.jsx
+++ client/src/components/organization/donation/Donation.jsx
@@ -3,10 +3,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getCompanies } from '../../../action/companyAction'
 import isEmpty from '../../../validation/isEmpty'
 import { CFormInput, CInputGroup, CInputGroupText, CForm, CButton } from '@coreui/react'
+import { AppContent, AppFooter, AppHeader, AppSidebar } from '../../index'
 
 export const Donation = () => {
-  const [total, setTotal] = React.useState(0);
-  const [displayState, setDisplayState] = React.useState(false);
+  const [total, setTotal] = React.useState(0)
+  const [displayState, setDisplayState] = React.useState(false)
 
   useEffect(() => {
     dispatch(getCompanies())
@@ -16,18 +17,13 @@ export const Donation = () => {
   let companyDonation = null
   if (!isEmpty(currentCompany.companies)) {
     console.log(currentCompany.companies[0])
-    companyDonation =
-      (
-        <div>
-            {currentCompany.companies[0].donation.map(
-          item => {
-              return (<h1>{item}</h1>)
-              }
-            )
-            }
-        </div>
-      )
-
+    companyDonation = (
+      <div>
+        {currentCompany.companies[0].donation.map((item) => {
+          return <h1>{item}</h1>
+        })}
+      </div>
+    )
   }
 
   const onChange = (event) => {
@@ -36,37 +32,42 @@ export const Donation = () => {
     })
   }
   let showDonation = (
-      <div>
-        <CInputGroup className="mb-3">
-
-          <CInputGroupText id="basic-addon1">
-            <p>Total Amount</p>
-          </CInputGroupText>
-          <CFormInput
-            placeholder="Donation Amount"
-            value={total}
-            onChange={()=>{}}
-          />
-          <CButton color="success" type="submit">
-            Donate
-          </CButton>
-          </CInputGroup>
-      </div>
-    )
-  return (
-    <>
-      <CForm onSubmit={()=>{}}>
-      <button
-        className="btn btn-light m-5"
-        type="button"
-        onClick={() => {setDisplayState(!displayState)
-          console.log({displayState})}
-        }>Donate Here!
-      </button>
-      {displayState ? showDonation: ''}
-      </CForm>
-    </>
-
+    <div>
+      <CInputGroup className="mb-3">
+        <CInputGroupText id="basic-addon1">
+          <p>Total Amount</p>
+        </CInputGroupText>
+        <CFormInput placeholder="Donation Amount" value={total} onChange={() => {}} />
+        <CButton color="success" type="submit">
+          Donate
+        </CButton>
+      </CInputGroup>
+    </div>
   )
-
+  return (
+    <div>
+      <AppSidebar />
+      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+        <AppHeader />
+        <div className="body flex-grow-1 px-3">
+          <>
+            <CForm onSubmit={() => {}}>
+              <button
+                className="btn btn-light m-5"
+                type="button"
+                onClick={() => {
+                  setDisplayState(!displayState)
+                  console.log({ displayState })
+                }}
+              >
+                Donate Here!
+              </button>
+              {displayState ? showDonation : ''}
+            </CForm>
+          </>
+        </div>
+        <AppFooter />
+      </div>
+    </div>
+  )
 }
