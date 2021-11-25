b6972851ec986233d899022c42ea3df52d54dfb2
Eroscupid <vutuanduong2003@yahoo.com>
2021-11-25 11:54:42 -0600

    commit change

--- client/src/action/companyAction.jsx
+++ client/src/action/companyAction.jsx
@@ -13,7 +13,7 @@ import {
 
 export const createCompany = (data, history) => (dispatch) => {
   axios
-    .post('/v1/company/', data)
+    .post('/service/v1/company/', data)
     .then((result) => {
       history.push('/dashboard')
     })
@@ -28,7 +28,7 @@ export const createCompany = (data, history) => (dispatch) => {
 // get Company
 export const getCompanies = () => (dispatch) => {
   axios
-    .get('/v1/company/all')
+    .get('/service/v1/company/all')
     .then((companies) =>
       dispatch({
         type: GET_COMPANIES,
@@ -47,7 +47,7 @@ export const getCompanies = () => (dispatch) => {
 export const getCurrentCompany = () => (dispatch) => {
   dispatch(setCompanyLoading())
   axios
-    .get('/v1/company/')
+    .get('/service/v1/company/')
     .then((result) =>
       dispatch({
         type: GET_CURRENT_COMPANY,
@@ -63,7 +63,7 @@ export const getCurrentCompany = () => (dispatch) => {
 } // get company with auth staff
 export const getCurrentCompanyByStaff = () => (dispatch) => {
   dispatch(setCompanyLoading())
-  axios.get('/v1/company/staff').then(
+  axios.get('/service/v1/company/staff').then(
     (result) =>
       dispatch({
         type: GET_CURRENT_COMPANY_BYSTAFF,
@@ -80,7 +80,7 @@ export const getCurrentCompanyByStaff = () => (dispatch) => {
 export const donate = (data) => (dispatch) => {
   if (window.confirm(`Are you sure you want to donate $${data.amount} to organization?`)) {
     axios
-      .post('/v1/company/donate', data)
+      .post('/service/v1/company/donate', data)
       .then((company) => {
         alert('donated!')
         dispatch({
@@ -101,7 +101,7 @@ export const donate = (data) => (dispatch) => {
 export const getCurrentCompanyById = (_id) => (dispatch) => {
   dispatch(setCompanyLoading())
   axios
-    .get(`/v1/company/${_id}`)
+    .get(`/service/v1/company/${_id}`)
     .then((result) =>
       dispatch({
         type: GET_CURRENT_COMPANY,
@@ -120,7 +120,7 @@ export const getCurrentCompanyById = (_id) => (dispatch) => {
 export const deleteCompany = (history) => (dispatch) => {
   if (window.confirm('Are you sure you want to delete your Company')) {
     axios
-      .delete('/v1/company/')
+      .delete('/service/v1/company/')
       .then((result) => {
         dispatch({
           type: DELETE_COMPANY,
@@ -140,7 +140,7 @@ export const removeStaff = (id) => (dispatch) => {
   if (window.confirm('Are you sure you want to remove your staff ?')) {
     // console.log(id)
     axios
-      .put(`/v1/company/${id}`)
+      .put(`/service/v1/company/${id}`)
       .then((result) => {
         console.log(result)
         window.location.reload()
